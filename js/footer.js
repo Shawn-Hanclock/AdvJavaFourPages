9a1980a84abab6ec18ebcccd455cc2d385bd9d96
Shawn-Hanclock <90814768+Shawn-Hanclock@users.noreply.github.com>
2024-01-09 07:47:53 +0000

    the start of content for page 1

--- js/footer.js
+++ js/footer.js
@@ -4,3 +4,12 @@ fetch('footer.html')
     document.getElementById('footer-container').innerHTML = html;
 })
 .catch(error => console.error('Error fetching footer:', error));
+
+// window.onscroll = function() {
+//   var footer = document.querySelector('footer');
+//   if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+//     footer.style.display = 'block';
+//   } else {
+//     footer.style.display = 'none';
+//   }
+// };
